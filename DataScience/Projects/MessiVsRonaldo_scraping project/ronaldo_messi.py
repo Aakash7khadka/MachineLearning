9d1dd126daee65b1c6d7df22a2eec003c057079a
aakash7khadka <aakash7khadka@gmail.com>
2025-02-25 13:29:47 +0100

    changes done due to change in wikepedia content

--- DataScience/Projects/MessiVsRonaldo_scraping project/ronaldo_messi.py
+++ DataScience/Projects/MessiVsRonaldo_scraping project/ronaldo_messi.py
@@ -9,13 +9,12 @@ import seaborn as sns
 import pandas as pd
 ron_url='https://en.wikipedia.org/wiki/Cristiano_Ronaldo'
 ronaldo=pd.read_html(ron_url,na_values='—')
-
+# print(ronaldo[7])
 
 # In[2]:
 
-
-ronaldo_goals=ronaldo[5].copy()
-ronaldo_goals
+#determine the table where the goals are situated i.e 7 currently
+ronaldo=ronaldo[7].copy()
 
 
 # In[3]:
@@ -29,86 +28,97 @@ messi=pd.read_html(mes_url,na_values='—')
 # In[5]:
 
 
-messi_goals=messi[4].copy()
-messi_goals
+messi=messi[5].copy()
 
 
 # In[6]:
 
 
-ronaldo_goals.columns=[col[1] for col in ronaldo_goals.columns]
-messi_goals.columns=[col[1] for col in messi_goals.columns]
+ronaldo.columns=[col[1] for col in ronaldo.columns]
+messi.columns=[col[1] for col in messi.columns]
 
 
 # In[7]:
 
 
-ronaldo_goals.drop('Apps',axis=1,inplace=True)
-messi_goals.drop('Apps',axis=1,inplace=True)
+print(ronaldo)
 
-ronaldo_goals['Season']=ronaldo_goals['Season'].str.replace(r'\[.*]','')
-messi_goals['Season']=messi_goals['Season'].str.replace(r'\[.*]','')
+ronaldo['Season']=ronaldo['Season'].str.replace(r'\[.*]','', regex= True)
+messi['Season']=messi['Season'].str.replace(r'\[.*\]','', regex= True)
+
+# 2004/05 to 2021/22[When both were playing for a full season in European Top 5 league]
+actual_range = [str(x)+'–'+str(x+1)[-2:] for x in range(2004, 2022)]
+print(actual_range)
+
+ronaldo = ronaldo[ronaldo['Season'].isin(actual_range)]
+messi = messi[messi['Season'].isin(actual_range)]
+
 
 
 # In[8]:
 
 
-ronaldo_goals=ronaldo_goals.set_index('Season').drop('Total').drop('Career total')
-messi_goals=messi_goals.set_index('Season').drop('Total').drop('Career total')
-
-ronaldo_goals=ronaldo_goals.iloc[:,-1]
-messi_goals=messi_goals.iloc[:,-1]
+ronaldo=ronaldo.set_index('Season')
+messi=messi.set_index('Season')
 
 
+ronaldo=ronaldo.iloc[:,-2:]
+messi=messi.iloc[:,-2:]
+
+# print(ronaldo)
 # In[9]:
 
-
-ronaldo_goals=ronaldo_goals.groupby(ronaldo_goals.index).sum()
-ronaldo_goals=ronaldo_goals.iloc[1:]
-
+# print(ronaldo_goals)
+# combining the multiple rows for same season
+ronaldo=ronaldo.groupby(ronaldo.index).sum()
+print(ronaldo)
+# ronaldo_goals=ronaldo_goals.iloc[1:]
+# print(ronaldo_goals)
 
 # In[21]:
 
 
-messi_goals=messi_goals.groupby(messi_goals.index).sum()
+# print(messi_goals.replace(r'\[\d+\]','',regex=True))
+# messi_goals = messi_goals.replace(r'\[\d+\]','',regex=True)
+messi=messi.groupby(messi.index).sum()
 
+print(messi)
+
+
+# print(messi_goals.reindex(actual_range))
 
 # In[27]:
 
 
-plt.style.available
+print(plt.style.available)
 
 
 # In[28]:
 
 
-plt.style.use('seaborn-colorblind')
+plt.style.use('seaborn-v0_8-colorblind')
 
 
-# In[41]:
-
-
-# plt.figure(figsize=(14,10))
-# ronaldo_goals.plot(label='Cristiano Ronaldo')
-# messi_goals.plot(color='#d31a63',label='Lionel Messi')
-# plt.legend()
-# plt.ylabel('Goals',fontsize=12)
-# plt.title('Club Career goals of Messi and Ronaldo (2003/04 - 2019/20) seasons',fontsize=15)
-
-
+# ronaldo_goals.name = 'ronaldo_goals'
+# messi_goals.name = 'messi_goals'
 # In[44]:
 
-
 plt.figure(figsize=(18,10))
-sns.lineplot(ronaldo_goals.index,ronaldo_goals,label='Cristiano Ronaldo')
-sns.lineplot(messi_goals.index,messi_goals,label='Lionel Messi')
+
+sns.lineplot(x=ronaldo.index,y=ronaldo['Goals'],label='Cristiano Ronaldo', dashes=(2,2))
+sns.lineplot(x=messi.index,y=messi['Goals'],label='Lionel Messi')
+plt.xticks(rotation=45)
 plt.legend()
 plt.ylabel('Goals',fontsize=12)
-plt.title('Club Career goals of Messi and Ronaldo (2003/04 - 2019/20) seasons',fontsize=15)
+plt.title('Club Career goals of Messi and Ronaldo (2003/04 - 2021/22) seasons',fontsize=15)
 
 
 # In[15]:
 
 
 plt.show()
-
+ronaldo.columns = ['ronaldo_apps', 'ronaldo_goals']
+messi.columns = ['messi_apps', 'messi_goals']
+print(ronaldo)
+combined_df = pd.concat([ronaldo, messi], axis=1)
+print(combined_df)
